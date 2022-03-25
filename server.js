db4768ee399c55edcbf946428373309b4ef222d4
Maryia <malakhava@yahoo.com>
2022-03-24 22:05:19 -0400

    fixes

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
-    sequelize.sync({ force: false });
+   // sequelize.sync({ force: false });
   });
